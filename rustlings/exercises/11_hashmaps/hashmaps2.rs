20403de020ca518f45d5096cc9c5268256667782
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-08-19 13:01:25 +0200

    Enhance hash map exercises by adding more fruit types and updating score calculations for soccer match results

--- rustlings/exercises/11_hashmaps/hashmaps2.rs
+++ rustlings/exercises/11_hashmaps/hashmaps2.rs
@@ -29,6 +29,23 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
+        match fruit {
+            Fruit::Apple => {
+                basket.insert(Fruit::Apple, 4);
+            }
+            Fruit::Banana => {
+                basket.insert(Fruit::Banana, 2);
+            }
+            Fruit::Mango => {
+                basket.insert(Fruit::Mango, 2);
+            }
+            Fruit::Lychee => {
+                basket.insert(Fruit::Lychee, 5);
+            }
+            Fruit::Pineapple => {
+                basket.insert(Fruit::Pineapple, 2);
+            }
+        }
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
